6c7660baae38d488bcde344163d57c08e968d9c0
migueldemh <miguel.heredero@hashicorp.com>
2023-01-27 12:06:28 +0100

    cleaning

--- variables.tf
+++ variables.tf
@@ -1,37 +1,24 @@
-
-variable "oauth_name" {
-  type        = string
-  description = "(Required String) Name of OAuth VCS connection you created"
-}
-variable "terraform_server" {
-  type        = string
-  description = "(Optional String) defaults to app.terraform.io, but otherwise the FQDN of your TFE server"
-  default     = "app.terraform.io"
-}
-variable "TFx_org_token" {
-  type        = string
-  description = "(Optional String) Token for the Terraform server organization level token"
+variable "tf_hostname" {
+  description = "Hostname of the Terraform instance."
+  type = string
+  default = "app.terraform.io"
 }
 
-variable "sentinel_vcs_repo_identifier" {
-  type        = string
-  description = "(Optional String) username/projectname of the VCS project or repository to source policy sets"
-  default     = "" # used as count flag
-}
-variable "sentinel_branch" {
-  type        = string
-  default     = "main"
-  description = "(Optional String) VCS repo branch defaults to main"
+variable "tf_token" {
+  description = "Terraform API Token."
+  type = string
+  default = "none"
 }
 
-variable "configure_policy_set" {
-  default     = false
-  type        = bool
-  description = "(Optional Bool) Flag to control deployment of policy sets."
+variable "tf_organization" {
+  description = "Name of the TF Organization"
+  type = string
+  default = "test-lloyds-demo"
 }
 
-variable "agent_pool_set" {
-  default     = false
-  type        = bool
-  description = "(Optional Bool) Flag to control of agent pool"
+variable "tf_workspaces" {
+  description = "Set of TF Workspace Names"
+  type = set(string)
+  default = ["workspaceA",
+    "workspaceB", "workspaceC"]
 }
--- workspace.tf
+++
@@ -1,8 +0,0 @@
-resource "tfe_workspace" "component-env" {
-  name           = "component-env"
-  organization   = local.organization
-  execution_mode = "local"
-  tag_names      = ["component", "vmp", "environment"]
-}
-
-
