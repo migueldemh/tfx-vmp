12ee34dbdb6e41e7a24d3f4fa972352fd7e5fa91
migueldemh <miguel.heredero@hashicorp.com>
2023-01-27 12:27:54 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,45 @@
-data "environment_variables" "atlas_slug" {
-  filter = "^ATLAS_WORKSPACE_SLUG"
+# SET A TF WORKSPACE VARIABLE FOR "tf_token" with the TFC API Token
+# https://www.terraform.io/docs/providers/tfe/index.html
+# Configure the Terraform Enterprise Provider
+terraform {
+  required_providers {
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.38.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.1.0"
+    }
+  }
 }
-locals {
-  list_organization_workspace = split("/", data.environment_variables.atlas_slug)
-  organization                = local.list_organization_workspace["0"]
-  workspace                   = local.list_organization_workspace["1"]
+
+provider "tfe" {
+  hostname = var.tf_hostname
+  token    = var.tf_token
+}
+
+
+
+resource "tfe_workspace" "test" {
+  for_each = var.tf_workspaces
+  name = each.key
+  organization = var.tf_organization
+}
+
+output "tf_workspace_ids" {
+  value = { for k, v in tfe_workspace.test : k => v.id }
+}
+
+resource "tfe_variable" "test" {
+  for_each = { for k, v in tfe_workspace.test : k => v.id }
+  key = "test_key_name"
+  value = "test_value_name"
+  category = "terraform"
+  workspace_id = each.value
+}
+
+resource "tfe_team" "test" {
+  name = "test-team-name"
+  organization = var.tf_organization
 }
