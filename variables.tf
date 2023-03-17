9b7a2c40e423f3f9c1497e37c20c2bd5b9ead065
migueldemh <miguel.heredero@hashicorp.com>
2023-03-17 08:20:08 +0100

    added org

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "TFx_org_token" {
 variable "tf_organization" {
   description = "Name of the TF Organization"
   type = string
-  default = "<ADD-ORG-NAME-FROM-TFC>"
+  default = "bupa-test-org"
 }
 
 variable "tf_workspaces" {
