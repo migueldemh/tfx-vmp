c3caaee72d13104a731227955625ff3368915476
migueldemh <miguel.heredero@hashicorp.com>
2023-09-05 17:03:46 +0200

    delete user

--- main.tf
+++ main.tf
@@ -97,10 +97,7 @@ resource "tfe_team_access" "app-dev-admin" {
   workspace_id = each.value
 }
 
-resource "tfe_team_member" "external" {
-  team_id  = tfe_team.app-dev-team.id
-  username = "miguelheredero"
-}
+
 
 output "tf_workspace_ids" {
   value = { for k, v in tfe_workspace.test : k => v.id }
