c05496ea4c4887ce3ad1d97a0e26f50a8a20cd7a
migueldemh <miguel.heredero@hashicorp.com>
2023-12-05 14:56:24 +0100

    resources

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-# SET A TF WORKSPACE VARIABLE FOR "tf_token" with the TFC API Token
-# https://www.terraform.io/docs/providers/tfe/index.html
-# Configure the Terraform Enterprise Provider
 terraform {
   required_providers {
     tfe = {
